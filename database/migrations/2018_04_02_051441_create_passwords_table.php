04a555a412a2a819449d4d5ab3df852610fe48f1
Vijay Singh <vijay.singh@planetwebsolution.com>
2018-04-02 11:07:00 +0530

    project setup

---
+++ database/migrations/2018_04_02_051441_create_passwords_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('website');
+            $table->string('username');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passwords');
+    }
+}
