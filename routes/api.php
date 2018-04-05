a8027f406903f4a824c6f9a3081eb475aad3857c
Vijay Bajrot <vijaybajrot@gmail.com>
2018-04-05 22:26:40 +0530

    cors problem solved

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/password', "PasswordController");
+Route::get('/password', "PasswordController@index");
